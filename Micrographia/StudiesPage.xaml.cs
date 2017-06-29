be56f37d129f78f33c2861fc305194ed2f78beb1
Mario Garcia Valdez <mariosky@gmail.com>
2017-06-28 19:17:54 -0700

    New Page Style Windows

--- Micrographia/StudiesPage.xaml.cs
+++ Micrographia/StudiesPage.xaml.cs
@@ -1,35 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-
 using System.Linq;
 using System.Text;
-
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-
-
-
-
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace Micrographia
 {
-    
-
-
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for StudiesPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class StudiesPage : Page
     {
-        public MainWindow()
+        public StudiesPage()
         {
             InitializeComponent();
-
-            _mainFrame.Navigate(new MediaPage());
         }
     }
 }
